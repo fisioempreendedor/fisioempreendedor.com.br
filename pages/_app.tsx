6f1aeec1d2a7ea1c9c8f70c41155c0bf9f3c101d
twobanks <twobanks@me.com>
2023-11-20 17:52:09 -0300

    remove useState

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,45 +2,31 @@ import { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import { useEffect, useState } from 'react';
 import '@/styles/index.css'
 import '@/styles/global.css'
 import Head from 'next/head';
 import { Auth0Provider } from '@auth0/auth0-react'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const [showing, setShowing] = useState(false);
-  useEffect(() => {
-    setShowing(true);
-  }, []);
-
-  if (!showing) {
-    return null;
-  }
-
-  if (typeof window === 'undefined') {
-    return <></>;
-  } else {
-    return (
-      <Auth0Provider
-        clientId={String(process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID)}
-        domain={String(process.env.NEXT_PUBLIC_AUTH0_DOMAIN)}
-      >
-        <Head>
-          <link rel="shortcut icon" href="/favicon/favicon.ico" />
-          <link rel="apple-touch-icon" href="/favicon/favicon-32x32.png" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        </Head>
-        <NextUIProvider>
-          <NextThemesProvider
-            attribute='class'
-            defaultTheme='dark'
-          >
-            <Component {...pageProps} />  
-            <Analytics />
-          </NextThemesProvider>
-        </NextUIProvider>
-      </Auth0Provider>
-    )
-  }
+  return (
+    <Auth0Provider
+      clientId={String(process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID)}
+      domain={String(process.env.NEXT_PUBLIC_AUTH0_DOMAIN)}
+    >
+      <Head>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon/favicon-32x32.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <NextUIProvider>
+        <NextThemesProvider
+          attribute='class'
+          defaultTheme='dark'
+        >
+          <Component {...pageProps} />  
+          <Analytics />
+        </NextThemesProvider>
+      </NextUIProvider>
+    </Auth0Provider>
+  )
 }
