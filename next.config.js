c55c5b7b13168a2cf19ec5923e1659ffbe995777
twobanks <twobanks@me.com>
2023-11-21 12:14:37 -0300

    img nextconfig

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
         hostname: 'platform-lookaside.fbsbx.com',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**'
+      }
     ],
   },
   reactStrictMode: true,
